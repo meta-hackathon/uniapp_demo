b4056cc8b0d65b7711af1540005f6c58b6203659
Edison <zhangjin_va@qq.com>
2022-12-02 17:32:05 +0000

    删除文件 unpackage/dist/dev

--- unpackage/dist/dev/mp-weixin/threex.js
+++
@@ -1,3 +0,0 @@
-(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["threex"],[],[[76,"common/runtime","common/vendor"]]]);
-//# sourceMappingURL=../.sourcemap/mp-weixin/threex.js.map
-module.exports = wx.__webpack_require_UNI_MP_PLUGIN__('76');
